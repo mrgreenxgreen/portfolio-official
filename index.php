560957f88a683563451f10fe0fc78d62e76c0602
unknown <davedalimocon@gmail.com>
2023-06-08 21:36:52 +0800

    .

---
+++ index.php
@@ -0,0 +1,152 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <!-- <meta name="viewport" content="width=device-width, initial-scale=1.0"> -->
+        <title>Circle Avatar</title>
+        <link rel="stylesheet" href="PageOne.css" /> 
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/simple-line-icons/2.5.5/css/simple-line-icons.min.css">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Cinzel:wght@500&display=swap" rel="stylesheet">
+    </head>
+    <body>
+        <!--Top Navigation Bar-->
+        <div class="top-navigation">
+            <div class="center-menu">
+                <div id="avatar">
+                    <img src="profpic.png" height="100" width="100"/>
+                </div>
+                <div class="name-bar">
+                    <div class="name-bar-name">
+                        <h1>Romeo Dave K.    Dalimocon</h1>
+                    </div>
+                   <div class="name-bar-position">
+                        <h3>Computer Instructor</h3>
+                   </div>
+                </div>
+            </div>
+            <div class="settings">
+                    <!-- <img src="settings.png" alt="SVG Image" height="80" width="80"> -->
+                    <!-- No need na settings,   minimalist-->
+            </div>
+        </div>
+        <!-- end of Top Navigation Bar-->
+
+        <!--  Left Bar : contact details-->
+        <!-- <div class="Left-Bar">  
+            <hr/>
+            <div>
+                <a href="https://www.linkedin.com/in/davedalimocon/">
+                    <span class="icon-social-linkedin"> linkedin.com/in/davedalimocon</span>
+                </a>
+                <br/>
+            </div>
+            <hr/>
+            <div>
+                <a href="https://mail.google.com/mail/u/0/#inbox?compose=new">
+                    <span class="icon-envelope"> davedalimocon@gmail.com </span>
+                </a>
+                <br/>
+            </div>
+            <hr/>
+            <div>
+                <a href="https://github.com/mrgreenxgreen">
+                    <span class="icon-social-github"> github.com/mrgreenxgreen </span>
+                </a>
+                <br/>
+            </div>
+            <hr/>
+        </div> -->
+        <!--  end of Left Bar : contact details-->
+
+        <!-- Main Content -->
+        <div class="invoker">
+            <img src="Invoker.png" alt="Invoker" style =""/>
+        </div>
+        <div class="container" >
+            <div class="icon">
+                <div class="imgBx" style="--i:1">
+                    <img src="react_glow.png" alt="SVG Image" height="200" widt="200">
+                </div>   
+                <div class="imgBx" style="--i:2">
+                    <img src="java_glow.png" alt="SVG Image" height="200" widt="200">
+                </div>   
+                <div class="imgBx" style="--i:3">
+                    <img src="Red_Hat_logo.png" alt="SVG Image" height="200" widt="200">
+                </div>    
+                <div class="imgBx" style="--i:4">
+                    <img src="aws.png" alt="SVG Image" height="200" widt="200">
+                </div>     
+                <div class="imgBx" style="--i:5">    
+                    <img src="mysql_glow.png" alt="SVG Image" height="200" widt="200">
+                </div>      
+                <div class="imgBx" style="--i:6">    
+                    <img src="springboot_glow.png" alt="SVG Image" height="200" widt="200">
+                </div>     
+                <div class="imgBx" style="--i:7">
+                    <img src="mongodb_glow.png" alt="SVG Image" height="200" widt="200">
+                </div>     
+                <div class="imgBx" style="--i:8">    
+                    <img src="nodejs_circle.png" alt="SVG Image" height="200" widt="200">
+                </div>
+                <div class="imgBx" style="--i:9">    
+                    <img src="psd_glow.png" alt="SVG Image" height="200" widt="200">
+                </div>
+            </div>   
+            
+        </div>
+        <!-- end of Main Content-->
+
+        <!-- bottom right navigation-->
+        <div class="right_nav" style="  position:fixed; right:10px; display:flex; flex-direction:column;bottom:30%;text-decoration: none;"> 
+            <div style=" text-decoration: none; height:80px;width:80px;border-radius:15px;font-size:15px;border:2px solid rgb(110, 110, 107);text-align:center;display:flex;flex-direction: column;justify-content: center;">
+           
+        
+                <a href="https://www.linkedin.com/in/davedalimocon/" style="text-decoration: none;">
+                    <span class="icon-social-linkedin"> </span>
+                </a>
+                
+            </div>
+             <div style="   height:80px;width:80px;border-radius:15px ;font-size:15px;border:2px solid rgb(110, 110, 107);text-align:center;display:flex;flex-direction:column;justify-content: center;">
+          
+              
+                <a href="https://github.com/mrgreenxgreen">
+                    <span class="icon-social-github" style="text-decoration: none;"> </span>
+                </a>
+            </div>
+            <div style=" height:80px;width:80px;border-radius:15px;font-size:15px;border:2px solid rgb(110, 110, 107);text-align:center; display:flex;flex-direction:column;justify-content: center;   ">
+                <a href="https://mail.google.com/mail/u/0/#inbox?compose=new" style="text-decoration: none;">
+                    <span class="icon-envelope">  </span>
+                </a>
+            </div>
+        <!-- <div style="background-color: aqua; height:80px;width:80px;border-radius:15px; bottom:40%; right:10px;position:absolute;"> -->       
+        </div>
+        <!-- end of bottom right navigation-->
+       
+        <!-- bottom menu slider-->
+        <div class="album" style="color:white;text-align: center;margin:auto;
+        position:absolute;bottom:0;
+         height: 130px;display:flex;align-items: center;justify-content:center; z-index:150;position:fixed;">
+
+            <div class="album-one" style="width:300px;
+            background-color: red;margin:3px;">
+                project 1
+            </div>
+            <div class="album-two" style="width:300px;
+            background-color: red;margin:3px;">
+                project 2
+            </div>
+            <div class="album-three" style="width:300px;
+            background-color: red;margin:3px;">
+                project 3
+            </div>
+            <div class="album-four" style="width:300px;
+            background-color: red;margin:3px;">
+                project 4
+            </div>
+           
+        <!-- end of bottom menu slider-->
+        </div>
+    </body>
+</html>
